6109de0b840a9b5624a6efb78d258882feb5a94e
Karly Mareka <karly.mareka@gmail.com>
2018-05-28 17:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
-    array.push(`${musicans[i]} plays ${instruments[i]})
+    array.push(`${musicans[i]} plays ${instruments[i]}`)
   }
 }
